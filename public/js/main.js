6654ceb1f37b88e8013cdea5e2b29b0fd27e0b3e
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-12 17:23:11 +0900

    initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -3,14 +3,15 @@ window.onload = () => {
     const myLiffId = '1654951421-nwJ0jYeb';
     const divLogin = document.getElementById('login_area');
 
-    // const connection = new Client({
-    //     user:process.env.PG_USER,
-    //     host:process.env.PG_HOST,
-    //     database:process.env.PG_DATABASE,
-    //     password:process.env.PG_PASSWORD,
-    //     port:5432
-    //   });
-    // connection.connect();
+    const { Client } = require('pg');
+    const connection = new Client({
+        user:process.env.PG_USER,
+        host:process.env.PG_HOST,
+        database:process.env.PG_DATABASE,
+        password:process.env.PG_PASSWORD,
+        port:5432
+      });
+    connection.connect();
      
     liff
         .init({
