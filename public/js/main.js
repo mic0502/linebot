19b5bb4d98b4ac76ce8bcb6fd11297e25490a8f9
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-14 08:16:30 +0900

    initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -15,100 +15,96 @@ window.onload = () => {
                     nameElement.innerHTML = profile.displayName + 'さま';
 
                     fetch(`api/link?line_uid=${lineId}`,{method:'GET'})
-                        .then(response=>{response.text()
-                            .then(text=>{
-                                if(isJson(text)){
-                                    // リンクトークン未発行。連携済みの場合顧客データを取得する
-                                    const linkToken = text;
+                        .then(response=>{
+                            if(isJson(response)){
+                                // リンクトークン未発行。連携済みの場合顧客データを取得する
+                                const linkToken = response;
 
-                                    const idElement = document.getElementById('lineid');
-                                    idElement.innerHTML = linkToken;
+                                const idElement = document.getElementById('lineid');
+                                idElement.innerHTML = linkToken;
 
-                                    const rankElement = document.getElementById('customer_rank');
-                                    const pointElement = document.getElementById('customer_point');
-                                    rankElement.innerHTML = '現在のランクは：' + JSON.parse(linkToken).rank + 'です。';
-                                    pointElement.innerHTML = '現在の保有ポイント：' + JSON.parse(linkToken).point + 'pt';
-                                }else{
-                                    // リンクトークン発行。未連携の場合    
-                                    const linkToken = text;
-                                    const idElement = document.getElementById('lineid');
-                                    idElement.innerHTML = linkToken;
+                                const rankElement = document.getElementById('customer_rank');
+                                const pointElement = document.getElementById('customer_point');
+                                rankElement.innerHTML = '現在のランクは：' + JSON.parse(linkToken).rank + 'です。';
+                                pointElement.innerHTML = '現在の保有ポイント：' + JSON.parse(linkToken).point + 'pt';
+                            }else{
+                                // リンクトークン発行。未連携の場合    
+                                const linkToken = response;
+                                const idElement = document.getElementById('lineid');
+                                idElement.innerHTML = linkToken;
 
-                                    const formElement = document.createElement('form');
-                                    formElement.setAttribute('id','login');
-                                    formElement.setAttribute('name','login_info');
-                                    formElement.setAttribute('method','post');
-                                    formElement.setAttribute('action','/api/users/login');　//POST先のアドレス
-        
-                                    // div_form1はログインIDに関するlabel要素とinput要素で構成
-                                    const div_form1 = document.createElement('div');
-        
-                                    const label_form1 = document.createElement('label');
-                                    label_form1.setAttribute('class','label_id');
-                                    label_form1.textContent = 'ログインID';
-                                    div_form1.appendChild(label_form1);
-        
-                                    const input_form1 = document.createElement('input');
-                                    input_form1.setAttribute('type','text');
-                                    input_form1.setAttribute('class','id-input');
-                                    input_form1.setAttribute('name','id');
-                                    div_form1.appendChild(input_form1);
-        
-                                    // div_form2はパスワードに関するlabel要素とinput要素で構成
-                                    const div_form2 = document.createElement('div');
-        
-                                    const label_form2 = document.createElement('label');
-                                    label_form2.setAttribute('class','label_password');
-                                    label_form2.textContent = 'パスワード';
-                                    div_form2.appendChild(label_form2);
-        
-                                    const input_form2 = document.createElement('input');
-                                    input_form2.setAttribute('type','password');
-                                    input_form2.setAttribute('class','password-input');
-                                    input_form2.setAttribute('name','password');
-                                    div_form2.appendChild(input_form2);
-        
-                                    // ログインボタン
-                                    const loginButton = document.createElement('input');
-                                    loginButton.value = 'ログイン';
-                                    loginButton.type = 'button';
-                                    loginButton.addEventListener('click',()=>{
-                                        const data = new FormData(formElement);
-                                        data.append('linkToken',linkToken);
-                                        console.log(...data.entries());
-        
-                                        fetch('/api/users/login',{
-                                            method:'POST',
-                                            body: data,
-                                            credentials: 'same-origin'
-                                        })
-                                        .then(response=>{
-                                            if(response.ok){
-                                                response.text()
-                                                    .then(text=>{
-                                                        const url = `https://access.line.me/dialog/bot/${text}`;
-                                                        console.log('url:',url);
-                                                        document.location.href = url;
-                                                    })
-                                            }else{
-                                                alert('HTTPレスポンスエラーです');
-                                            }
-                                        })
-                                        .catch(e=>console.log(e));
-                                    });
-        
-                                    // フォーム要素にform1,form2,loginButtonを格納
-                                    formElement.appendChild(div_form1);
-                                    formElement.appendChild(div_form2);
-                                    formElement.appendChild(loginButton);
-        
-                                    // フォーム要素を大元のdiv要素へ格納
-                                    divLogin.appendChild(formElement);
-        
-                                }
-                            });
-
-
+                                const formElement = document.createElement('form');
+                                formElement.setAttribute('id','login');
+                                formElement.setAttribute('name','login_info');
+                                formElement.setAttribute('method','post');
+                                formElement.setAttribute('action','/api/users/login');　//POST先のアドレス
+    
+                                // div_form1はログインIDに関するlabel要素とinput要素で構成
+                                const div_form1 = document.createElement('div');
+    
+                                const label_form1 = document.createElement('label');
+                                label_form1.setAttribute('class','label_id');
+                                label_form1.textContent = 'ログインID';
+                                div_form1.appendChild(label_form1);
+    
+                                const input_form1 = document.createElement('input');
+                                input_form1.setAttribute('type','text');
+                                input_form1.setAttribute('class','id-input');
+                                input_form1.setAttribute('name','id');
+                                div_form1.appendChild(input_form1);
+    
+                                // div_form2はパスワードに関するlabel要素とinput要素で構成
+                                const div_form2 = document.createElement('div');
+    
+                                const label_form2 = document.createElement('label');
+                                label_form2.setAttribute('class','label_password');
+                                label_form2.textContent = 'パスワード';
+                                div_form2.appendChild(label_form2);
+    
+                                const input_form2 = document.createElement('input');
+                                input_form2.setAttribute('type','password');
+                                input_form2.setAttribute('class','password-input');
+                                input_form2.setAttribute('name','password');
+                                div_form2.appendChild(input_form2);
+    
+                                // ログインボタン
+                                const loginButton = document.createElement('input');
+                                loginButton.value = 'ログイン';
+                                loginButton.type = 'button';
+                                loginButton.addEventListener('click',()=>{
+                                    const data = new FormData(formElement);
+                                    data.append('linkToken',linkToken);
+                                    console.log(...data.entries());
+    
+                                    fetch('/api/users/login',{
+                                        method:'POST',
+                                        body: data,
+                                        credentials: 'same-origin'
+                                    })
+                                    .then(response=>{
+                                        if(response.ok){
+                                            response.text()
+                                                .then(text=>{
+                                                    const url = `https://access.line.me/dialog/bot/${text}`;
+                                                    console.log('url:',url);
+                                                    document.location.href = url;
+                                                })
+                                        }else{
+                                            alert('HTTPレスポンスエラーです');
+                                        }
+                                    })
+                                    .catch(e=>console.log(e));
+                                });
+    
+                                // フォーム要素にform1,form2,loginButtonを格納
+                                formElement.appendChild(div_form1);
+                                formElement.appendChild(div_form2);
+                                formElement.appendChild(loginButton);
+    
+                                // フォーム要素を大元のdiv要素へ格納
+                                divLogin.appendChild(formElement);
+    
+                            }
 
                         });
                                
