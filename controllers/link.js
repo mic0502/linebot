a152ac6f1aec0bef16810f766b0ecd3391bcd926
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-13 14:10:41 +0900

    initial commit

--- controllers/link.js
+++ controllers/link.js
@@ -15,22 +15,20 @@ module.exports = {
                 console.log('登録済みアカウント');
             }else{
                 console.log('未登録アカウント');
+                const options = {
+                    url:`https://api.line.me/v2/bot/user/${line_uid}/linkToken`,
+                    method:'POST',
+                    headers:{'Authorization':'Bearer ahd1DH4XRUUjgL11hcQUMQxPXS4Xcr8UU1KOAzKIokK6LVe1I/ERSJ7fh8Epp8vLPrH+nB3oz52G0X3uBZpSvlxU74lkJJgY3oGQ4lc8ApLARAKN/7KOeIFNp1PdjXJ5XsNbxJLNDuQxB3YunWUJBQdB04t89/1O/w1cDnyilFU='},
+                    followAllRedirects: false
+                }
+        
+                request(options)
+                    .then(body=>{
+                        const parsedBody = JSON.parse(body);
+                        res.status(200).send(parsedBody["linkToken"]);
+                    });
+        
             }
         })
-
-        console.log('テスト通過３');
-        console.log('line_uid:',line_uid);
-        const options = {
-            url:`https://api.line.me/v2/bot/user/${line_uid}/linkToken`,
-            method:'POST',
-            headers:{'Authorization':'Bearer ahd1DH4XRUUjgL11hcQUMQxPXS4Xcr8UU1KOAzKIokK6LVe1I/ERSJ7fh8Epp8vLPrH+nB3oz52G0X3uBZpSvlxU74lkJJgY3oGQ4lc8ApLARAKN/7KOeIFNp1PdjXJ5XsNbxJLNDuQxB3YunWUJBQdB04t89/1O/w1cDnyilFU='},
-            followAllRedirects: false
-        }
-
-        request(options)
-            .then(body=>{
-                const parsedBody = JSON.parse(body);
-                res.status(200).send(parsedBody["linkToken"]);
-            });
     }
 }
