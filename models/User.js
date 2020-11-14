6b5d6750850e614689957c8c2cd32cd61fd2b427
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-14 11:08:56 +0900

    initial commit

--- models/User.js
+++ models/User.js
@@ -33,7 +33,7 @@ module.exports = {
             console.log('createUser:',createUser);
 
             const rank = 'D';
-            const point = 0;
+            const point = '0';
             const insert_query = {
                 text:`INSERT INTO users (name,login_id,login_password,rank,point) VALUES($1,$2,$3,'${rank}','${point}');`,
                 values:createUser
