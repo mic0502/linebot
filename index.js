b69b0d1700a2dc7c341c694b73ec2937323e95a0
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-19 16:15:16 +0900

    initial commit

--- index.js
+++ index.js
@@ -79,18 +79,26 @@ app
         const select_query = {text:`SELECT * FROM users WHERE line_id='${lineId}';`}
         User.check(select_query)
             .then(checkRes=>{
-                const name = checkRes.rows[0].name;
-                const login_id = checkRes.rows[0].login_id;
-                const password = checkRes.rows[0].login_password;
-                const update_query = {text:`UPDATE users SET (name, login_id, login_password, line_id) = ('${name}', '${login_id}', '${password}', '') WHERE login_id='${login_id}';`}
-                
-                User.release(update_query)
-                .then(response=>{
+                if(checkRes.rowCount > 0){
+                    const name = checkRes.rows[0].name;
+                    const login_id = checkRes.rows[0].login_id;
+                    const password = checkRes.rows[0].login_password;
+                    const update_query = {text:`UPDATE users SET (name, login_id, login_password, line_id) = ('${name}', '${login_id}', '${password}', '') WHERE login_id='${login_id}';`}
+                    
+                    User.release(update_query)
+                    .then(response=>{
+                        return client.replyMessage(ev.replyToken,{
+                            "type":"text",
+                            "text":"連携が解除されました！"
+                        });
+                    })        
+                }else{
+                    // 未連携の場合
                     return client.replyMessage(ev.replyToken,{
                         "type":"text",
-                        "text":"連携が解除されました！"
+                        "text":`${profile.displayName}さん、今${text}って言いました？`
                     });
-                })        
+                }
             })
 
     }else{
