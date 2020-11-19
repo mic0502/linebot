b8285ade4b1dfc28e6c30eb3197756c89d28d174
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-19 14:58:42 +0900

    initial commit

--- controllers/link.js
+++ controllers/link.js
@@ -11,8 +11,8 @@ module.exports = {
             if (checkRes.rowCount > 0 ){
                 // すでに連携済の場合
                 console.log('登録済みアカウント');
-                console.log(checkRes[0].name);
-                res.status(200).send(checkRes[0]);
+                console.log(checkRes.rows[0].name);
+                res.status(200).send(checkRes.rows[0]);
             }else{
                 // まだ連携されていない場合リンクトークンを取得
                 console.log('未登録アカウント');
