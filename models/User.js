c22605eb8d3cd1acb36f8340cd71fec54adfb259
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-26 07:57:30 +0900

    initial commit

--- models/User.js
+++ models/User.js
@@ -19,7 +19,7 @@ module.exports = {
 
     check:(select_query)=>{
         return new Promise((resolve,reject)=>{
-            const select_query = {text:`SELECT * FROM users WHERE line_id='111111';`};
+            console.log(select_query);
             connection.query(select_query, (error, results, fields) => {
                 if (error){ reject(error)}
                     console.log('データベースcheck成功');
