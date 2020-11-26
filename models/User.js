a03c06c771675953b20b6a6ac7f3a3a81a380176
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-26 19:12:54 +0900

    initial commit

--- models/User.js
+++ models/User.js
@@ -44,7 +44,7 @@ module.exports = {
             const select_query = `SELECT * FROM nonces WHERE nonce='${nonce}';`
             connection.query(select_query, (error, results, fields) => {
                 if (error){reject(error)};
-                    const login_id = results.rows[0].login_id;
+                    const login_id = results[0].login_id;
                     const update_query = `UPDATE users SET (login_id, line_id) = ('${login_id}', '${lineId}') WHERE login_id='${login_id}';`
                     connection.query(update_query, (error, results1) => {
                         if (error){reject(error)};
