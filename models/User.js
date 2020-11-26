0223ed84591af3a4c186d11f73693da2529276cf
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2020-11-26 19:05:33 +0900

    initial commit

--- models/User.js
+++ models/User.js
@@ -10,7 +10,7 @@ module.exports = {
     create:(insert_query)=>{
         return new Promise((resolve,reject)=>{
             connection.query(insert_query, (error, results) => {
-                if (error) reject(error)
+                if (error){reject(error)};
                     console.log('データベースcreate成功');
                     resolve(results);
                 })
@@ -21,7 +21,7 @@ module.exports = {
         return new Promise((resolve,reject)=>{
             console.log(select_query);
             connection.query(select_query, (error, results, fields) => {
-                if (error){ reject(error)}
+                if (error){ reject(error)};
                     console.log('データベースcheck成功');
                     resolve(results);
             })
@@ -31,7 +31,7 @@ module.exports = {
     insertNonce:(insert_query,linkToken,nonce)=>{
         return new Promise((resolve,reject)=>{
             connection.query(insert_query, (error, results) => {
-                if (error) reject(error)
+                if (error){reject(error)};
                     console.log('データベースinsertNonce成功');
                     const linkSentence = `accountLink?linkToken=${linkToken}&nonce=${nonce}`;
                     resolve(linkSentence);
@@ -43,11 +43,11 @@ module.exports = {
         return new Promise((resolve,reject)=>{
             const select_query = `SELECT * FROM nonces WHERE nonce='${nonce}';`
             connection.query(select_query, (error, results, fields) => {
-                if (error) reject(error)
+                if (error){reject(error)};
                     const login_id = results.rows[0].login_id;
                     const update_query = `UPDATE users SET (login_id, line_id) = ('${login_id}', '${lineId}') WHERE login_id='${login_id}';`
                     connection.query(update_query, (error, results1) => {
-                        if (error) reject(error)
+                        if (error){reject(error)};
                             console.log('データベースlink成功');
                             resolve(results1);
                         })        
