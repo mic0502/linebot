e84d0f5ddb56073a35c6e5bd728730bdc8e7815b
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-03 12:15:34 +0900

    Initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 window.onload = () => {
 
-    const myLiffId = '1654951421-nwJ0jYeb';  //本番
-    if(process.env.ENV_PATH.indexOf('heroku')>0){
-        myLiffId = '1654951421-JM6vmP1n';  //テスト
-    }
-
+    // const myLiffId = '1654951421-nwJ0jYeb';  //本番
+    // if(process.env.ENV_PATH.indexOf('heroku')>0){
+    //     myLiffId = '1654951421-JM6vmP1n';  //テスト
+    // }
+    // const myLiffId = '1654951421-nwJ0jYeb';  //本番
+    const myLiffId = '1654951421-JM6vmP1n';  //テスト
     const divLogin = document.getElementById('login_area');
     const param = new URL(location).search;
 
