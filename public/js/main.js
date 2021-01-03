0c1aa2f7e7917d97f53218ea5918fd1243a51948
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-04 02:45:48 +0900

    Initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,7 @@
 window.onload = () => {
 
-    require('dotenv').config();
-    const myLiffId = process.env.ENV_MYLIFFID;
     // const myLiffId = '1654951421-nwJ0jYeb';  //本番
-    // const myLiffId = '1654951421-JM6vmP1n';  //テスト
+    const myLiffId = '1654951421-JM6vmP1n';  //テスト
     const divLogin = document.getElementById('login_area');
     const param = new URL(location).search;
     alert(myLiffId);
