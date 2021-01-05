872993a67c4ada5babbf331d0762981fea803d4b
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-05 15:58:01 +0900

    Initial commit

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const handleMessageEvent = async (ev) => {
     const text = (ev.message.type === 'text') ? ev.message.text : '';
     
     if(text === '予約する'){
-        orderChoice(ev.replyToken);
+        orderChoice(ev);
     }else{
         return client.replyMessage(ev.replyToken,{
             "type":"text",
