b3d3bd61b4d1c4254c074650c5902b33b07d516f
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-11 17:18:45 +0900

    Initial commit

--- public/js/users.js
+++ public/js/users.js
@@ -237,7 +237,6 @@
         divHeader.onmousedown = (e) =>{
             let shiftX = e.clientX - divCard.getBoundingClientRect().left;
             let shiftY = e.clientY - divCard.getBoundingClientRect().top;
-            alert(`${shiftX}/${shiftY}`)
             const moveAt = (pageX,pageY) => {
                 if(pageX-shiftX>=0){
                     divCard.style.left = pageX - shiftX + 'px';
@@ -252,6 +251,7 @@
             }
             
             moveAt(e.pageX,e.pageY);
+            alert(`${moveAt}/${pageX,pageY}`)
             
             const onMouseMove = (e) => {
                 moveAt(e.pageX,e.pageY);
