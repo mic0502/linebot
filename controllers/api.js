8c289ecfb888313e38c643aac11b734cad08de80
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-12 08:39:29 +0900

    Initial commit

--- controllers/api.js
+++ controllers/api.js
@@ -5,16 +5,15 @@ module.exports = {
     getData: (req,res) => {
         const pickup_users = 'SELECT * FROM TM_KOK;';
         const pickup_reservations = 'SELECT * FROM TM_RESERVE;';
-        console.log('通過０')
         User.dbQuery(pickup_users,'顧客情報照会')
             .then(users=>{
                 User.dbQuery(pickup_reservations,'予約情報照会')
                     .then(reservations=>{
-                        console.log('通過１')
                         const data = {
                             users:users,
                             reservations:reservations
                         }
+                        console.log('通過１')
                         res.status(200).json(data);
                     })
                     .catch(e=>console.log(e))
