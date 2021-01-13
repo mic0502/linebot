b403897326a81a4608788062e293cff966f5eb0b
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-13 16:10:23 +0900

    Initial commit

--- public/js/users.js
+++ public/js/users.js
@@ -232,9 +232,8 @@
                     response.json()
                     .then(data=>{
                         // 画面を更新する
-                        const row8 = document.getElementsByName("row-menu");
                         // response.selecteddate + ' ' + response.selectedtime;
-                        row8[0].textContent = data.menu;
+                        document.getElementsByName("row-menu")[0].textContent = data.menu;
                         divCard.style.display = 'none';
                         alert('更新完了！');
                     })        
