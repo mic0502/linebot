c46de0e85bf2a38dccb799f0e436dc8255df2c38
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-20 18:38:46 +0900

    Initial commit

--- public/js/main.js
+++ public/js/main.js
@@ -55,7 +55,10 @@ window.onload = () => {
                                     document.getElementById('customer_medal').appendChild(medal_img);
                                     document.getElementById('customer_rankup').appendChild(rankup_img);
 
-                                    document.getElementById('barcode').setAttribute('class','aaaa');
+                                    const barcode_form = document.createElement('svg');
+                                    barcode_form.setAttribute('id','barcode');
+                                    barcode_form.setAttribute('jsbarcode-value','201508044');
+                                    document.getElementById('wrap_barcode').appendChild(barcode_form);
 
                                                                                                             
                                 }else{
