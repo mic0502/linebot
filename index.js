8369735f48e059e940e670fc283e5127be7998aa
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-21 16:23:12 +0900

    Initial commit

--- index.js
+++ index.js
@@ -126,7 +126,6 @@ const handlePostbackEvent = async (ev) => {
   }else if(splitData[1] === 'end'){
         pushText = {"type":"text","text":'予約を中止しました。'};
   }else if(splitData[1] === 'delete'){
-    console.log('テスト' + parseInt(splitData[2]))
     pushText = await reserve.deleteReserve(parseInt(splitData[2]));
   }
   return client.replyMessage(ev.replyToken,pushText);
