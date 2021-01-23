225b09a3551771129e0324064ac74b6673b30b4f
三原 満 <Michiru@mihara-no-MacBook-Air.local>
2021-01-23 18:02:15 +0900

    Initial commit

--- models/User.js
+++ models/User.js
@@ -1,32 +1,13 @@
 require('dotenv').config();
 const { response } = require('express');
 const mysql = require('mysql')
-const db_config = mysql.createConnection({
+const connect_config = {
     host:process.env.ENV_HOST,
     database:process.env.ENV_DATABASE,
     user:process.env.ENV_USER,
     password:process.env.ENV_PASSWORD
-});
-
-function handleDisconnect() {
-    connection = mysql.createConnection(db_config);
-    //connection取得
-    connection.connect(function(err) {
-        if (err) {
-            setTimeout(handleDisconnect, 1000);
-        }
-    });
-    
-    //error('PROTOCOL_CONNECTION_LOST')時に再接続
-    connection.on('error', function(err) {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    });
 }
-handleDisconnect();
+const connection = mysql.createPool(connect_config);
 
 
 module.exports = {
@@ -41,6 +22,16 @@ module.exports = {
                     resolve(results);
                 };
             })
+            connection.on('error', function(err) {
+                if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+                    console.log('=> RECONECT...');
+                    //再接続
+                    connection = mysql.createPool(connect_config);
+                } else {
+                    throw err;
+                }              
+            });
+
         });
     }
 
